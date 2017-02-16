a53ed6535f110f99e22dddec085daa74db003ba5
Gustavo Ilha <guga_ilha99@hotmail.com>
2017-02-16 05:24:59 -0200

    Style added

--- ola.php
+++ ola.php
@@ -3,6 +3,12 @@
 <head>
   <meta charset="utf-8"/>
   <title>Primeiro Exemplo PHP</title>
+  <style>
+    h2 {
+      color: rgb(22, 148, 219);
+      text-shadow: 1px 1px 1px black;
+    }
+  </style>
 </head>
 <body>
   <h1>Testando PHP</h1>
