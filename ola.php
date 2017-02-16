1f2dae2a7b15f30b425cd6390da6cd2da1b3d76a
Gustavo Ilha <guga_ilha99@hotmail.com>
2017-02-16 05:05:20 -0200

    Ola Mundo

---
+++ ola.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>Primeiro Exemplo PHP</title>
+</head>
+<body>
+  <h1>Testando PHP</h1>
+  <?php
+    echo "Ola, Mundo!";
+   ?>
+</body>
+</html>
