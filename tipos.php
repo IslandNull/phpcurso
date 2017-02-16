7ce5426ba27d66087f3af4c68a368aa42a3e3fb0
Gustavo Ilha <guga_ilha99@hotmail.com>
2017-02-16 05:46:41 -0200

    Old e Tipos
    Ola movido para Old, e a criação da pagina tipos, para testes de
    variaveis.

---
+++ tipos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $n = 4;
+      $no = "Gustavo";
+      echo $no;
+    ?>
+  </body>
+</html>
