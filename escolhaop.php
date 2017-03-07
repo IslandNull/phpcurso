6330e21f0d79e69c8076ee4e9dc9250c50b33eda
Gustavo Ilha <guga_ilha99@hotmail.com>
2017-03-07 18:47:51 -0300

    Multiple changes!

---
+++ escolhaop.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="_css/estilo.css">
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+    <div>
+        <?php
+            $n = isset($_GET["num"])?$_GET["num"]:0;
+            $o = isset($_GET["oper"])?$_GET["oper"]:1;
+            switch ($o) {
+                case 1:
+                    $r = $n * 2;
+                    break;
+
+                case 2:
+                    $r = $n ^ 3;
+                    break;
+
+                case 3:
+                    $r = sqrt($n);
+
+                }
+            echo "O resultado da operação solicitado foi <span class='foco'>$r</span><br/>"
+        ?>
+        <a href="escolhaop.html" class="botao">Voltar</a>
+    </div>
+    </body>
+</html>
